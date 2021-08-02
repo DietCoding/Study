11f418593bb2d0988381e2b2badd598c8259729e
DietCoding <88271670+DietCoding@users.noreply.github.com>
2021-08-02 23:38:21 +0900

    0802

---
+++ C#/Csharp/Csharp/TryCatchException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+class TryCatchException
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            //실행 문장
+        }
+        catch(IndexOutOfRangeException ex)//배열 관련 예외 처리
+        {
+            //catch문에서 exception을 다시 상위 호출자로 보내고 싶을 때
+            throw new IndexOutOfRangeException("Invalid index", ex);
+            //새 객체의 매개변수로 이전 exception 정보를 저장 
+        }
+        catch(FileNotFoundException ex)
+        {
+            throw ex;
+            //일반적으로 사용하지 않는 것이 좋다.
+        }
+        catch(Exception ex)
+        {
+            throw;
+            //catch문에서 잡힌 exception 전달
+        }
+        finally
+        {
+            //exception 발생 여부와 상관없이 마지막에 반드시 실행
+            Console.WriteLine("나니모 나캇다!");
+        }
+    }
+}
+
---
+++ C#/Csharp/Csharp/YieldInuemerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MyList
+{
+    private int[] data = { 1, 2, 3, 4, 5 };
+    
+    public IEnumerator GetEnumerator()
+    {
+        int i = 0;
+        while ( i < data.Length)
+        {
+            yield return data[i];
+            i++;
+        }
+    }
+}
+
+class Yield
+{
+    static void Main(string[] args)
+    {
+        var list = new MyList();
+
+        //foreach문에서 자동으로 iterator 루프를 처리
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        //수동으로 IEnumerator을 생성해서 iteration
+        IEnumerator it = list.GetEnumerator();        
+        it.MoveNext();
+        Console.WriteLine(it.Current);
+        it.MoveNext();
+        Console.WriteLine(it.Current);
+    }
+}
+
